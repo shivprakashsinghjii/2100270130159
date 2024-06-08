f0e933f59bb3f0a3e223bd488a038f980f3691ee
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2024-06-08 11:25:01 +0530

    abc

---
+++ top-products/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductCard from "./components/ProductCard";
+import ProductList from "./components/ProductLIst";
+
+const App = () => {
+  return (
+    <>
+      <ProductCard />
+      <ProductList />
+    </>
+  );
+};
+
+export default App;
